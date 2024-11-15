a7939f3334abd7ca5ee5f41a85381ed2f08e30f9
Omar Sherif Shalan <o.sherif2247@nu.edu.eg>
2024-11-15 14:42:22 +0200

    Update and rename test_add.py to test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -1,11 +1,11 @@
 import unittest
-from add import add
+from addition import addition
 
 class TestAdd(unittest.TestCase):  
     def test_add(self):
-        result = add.add(1, 1)
+        result = addition.add(1, 1)
         self.assertEqual(result, 2)  
-        result = add.add(-1, -2)
+        result = addition.add(-1, -2)
         self.assertEqual(result, -3)  
 
 if __name__ == '__main__':
